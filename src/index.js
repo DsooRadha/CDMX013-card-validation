d458bd194c355828e856e847d453a67f405d1072
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2022-07-22 14:49:34 -0500

    operacionesenparesfinalizadas

--- src/index.js
+++ src/index.js
@@ -2,17 +2,29 @@ import validator from './validator.js';
 
 console.log(validator);
 
-let numeroDeTarjeta=document.getElementById("botonDeValidar");
-numeroDeTarjeta.addEventListener("click",function(e){
-    e.preventDefault()
-    let numeroDeTarjeta=document.getElementById("espacioParaElNumeroDeTarjeta").value;
-     if (numeroDeTarjeta=="") {
-        alert ("Introduce el numero de tarjeta como se muetra en la imagen de arriba")
-        }
-        
-    let resultValid= validator.isValid(numeroDeTarjeta)
+let numeroDeTarjeta = document.getElementById("botonDeValidar");
+numeroDeTarjeta.addEventListener("click", function (e) {
+    e.preventDefault() //evita que se corra la funcion por accidente como cuando recargas la pagina XD
+    let numeroDeTarjeta = document.getElementById("espacioParaElNumeroDeTarjeta").value; //el valorde ese elemento, que en este caso es el inputtext
+    if (numeroDeTarjeta == "") { //si este elemento esta vacio al darle click al boton arroja un alert 
+        alert("Introduce el numero de tarjeta como se muetra en la imagen de arriba")
+    }
+
+/*
+maskify.function(numeroDeTarjeta){
+        let cuatroDigitos = numeroDeTarjeta.slice(-4); //cuando es negativo empieza del final de la cadena .slice()
+        console.log(cuatroDigitos + "probandoslice");
+        let remplazo = numeroDeTarjeta.slice(0,-4);
+        let numerosEnmascarados= numeroDeTarjeta.replace(remplazo,"#".repeat(numeroDeTarjeta.length));
+       // return numerosEnmascarados
+        console.log(numerosEnmascarados +"numerosenmascardos");
+       
+
+*/
+
+    let resultValid = validator.isValid(numeroDeTarjeta)
     console.log(resultValid)
-}); 
+});
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,98 +1,105 @@
 // toda la logica 
 const validator = {
-  isValid:function(numeroDeTarjeta){
-    let numeroInvertido = numeroDeTarjeta.toString().split("").reverse().join("");
-    let digitos= numeroInvertido.toString().split('');
-    let numerosEnDigitos= digitos.map(Number)
-    console.log(numerosEnDigitos);   
-    
+  isValid: function (numeroDeTarjeta) {
+    let numeroInvertido = numeroDeTarjeta.split("").reverse().join(""); //
+    const arregloCadena = [...numeroInvertido]; /*Spreadarray solo estoy clonando. Si no lo usas y solo v1=v2 tu array original se ve afectado. 
+    Tiene mas uso: Fusionar, clonar, Iterables a matriz, puede convertir un objeto-set- a matriz*,___otroque no recuerd*/
+    console.log(arregloCadena);
+    let character = ''; //se escribe asi cuando va a ser un string con corchetes cuando es de number
+    for (let i = 0; i < numeroInvertido.length; i++) { //la estructura de los for es ([espresiónInicial]; [expresionCondicional]; [expresiondeActualización]) es decir desde donde va empezar a contar, la condicion y como va a ir avanzando
+      character = numeroInvertido.charAt(i); // devuelve en un nuevo String el carácter UTF-16 de una cadena. En este caso la posición
+      let numero = parseInt(character) //lo vuelve numero al valor de character
+      if (i % 2 == 0) { //aca es la condicion "si" la posicion la divides entre dos ===0 es par 
+        arregloCadena[i] = "PAR";
+        var pares = numero * 2;
+        if (pares > 9) {
+          pares = pares - 9;
+        } // Si tu imprimes la consola ante de darle todas las intrucciones te va imprimiendo hasta los pasos anteriores no el paso final
+        console.log(pares +"par");
+      }
+      else {   //todo lo que no entra en la condicion if lo escupe acá
+        console.log(numero +"impar");
+          //YA INTENTE ESTAS DOSS FORMAS DE ABAJO O Y NO FUNCIONA O___o
+        //let todosLosNumeros=[...numeros, ...pares];
+        //let todosLosNumeros=numero.concat(pares);//arr1 = [...arr1, ...arr2];
+        //console.log(todosLosNumeros);
+      }
+    }
+
+  
 /*
-    let Pares= numerosEnDigitos.map();
-    let operacionesPares= function (numerosEnDigitos){
+    function suma ()
+    */
+    console.log(arregloCadena);
 
-    }
 
+    /*     function sumaDePares(pares){
+                    let suma=0;
+                    for (let j = 0; j <pares.length; j++) { 
+                      if (pares>9) { 
+                        SUMA TODOS LOS ELEMENTOS XD --aqui iria la estructura de un contador--- CREO
+                    }
+            }
+            
+                    */
 
-function filtrarpares(numerosEnDigitos){
-  const pares=[];
-
-  for (let i=1; 1<numerosEnDigitos.length; i++){
-    if (digitos[i]%2===0){
-      pares.push(digitos)
-
-      return pares;
-    };
-  }
+  },
+  numFunctions: 2
 }
-*/
-    for (let i = 0 ; i < digitos.length; i++) {
-      if (digitos[i]%2===0);
-      
-     
-    }
-    
-    
-    
-    // [i] %2 == 0
-    // en el futuro true o false
-    return true
-  }
 
-};
+validator.isValid('4083952015263');
+
+
+// en el futuro true o false
 
 
 
-  // ...
+
+
+
+// ...
 
 
 export default validator;
 
 
 /*
-
 LO SIGUIENTE SERÍA...
-
     BUCLES
         var realDigits = digits.map(Number)
         Definiendo numeros pares if (variableDefinidaPreviamente [i] %2 == 0);
             for ([expresiónInicial]; [expresiónCondicional]; [expresiónDeActualización])
             instrucción
-
     Otro metodo podria ser .forEach
-
     variable.forEach(function(pares)){
       console.log (pares*2)
     }
        
     con IF el elemento tiene un valor= 2 --> n*2
-
      otro   IF el resultado de esta operacion es > 9 
-
         se volveria a segmentar o volver un arreglo   con   
         
                         n.split("")
         
         y el resultado= elemento1+elemento2
-
     LO SIGUIENTE SERIA...
-
         var sumarTodosLosElementos= elemento1 + elemento2 + elemento3 ...elemento16
         
             IF  sumarTodosLosElementos/10 = 0 -->True
             IF  sumarTodosLosElementos/10 ≠ 0 -->False
-
     RESULTADO
-
         true=Mostar el div de Continuar ¡Tu tarjeta es valida
         false=Mostrar el div de Intenta con otro tarjeta
-
         *existe  this.value?
-
         Este algoritmo LUHN
-
         - Obtenemos la reversa del número a verificar (que solamente contiene dígitos [0-9]);
         - a todos los números que ocupan una posición par se les debe multiplicar por dos,
         - si este número es mayor o igual a 10, debemos sumar los dígitos del resultado; 
         -el número a verificar será válido si la suma de sus dígitos finales es un múltiplo de 10.
-        
+
+    FUNCION COLLBACK  ---.MAP
+    Los callbacks aseguran que una función no se va a ejecutar antes de que se complete una tarea, 
+    sino que se ejecutará justo después de que la tarea se haya completado. Nos ayuda a desarrollar código JavaScript 
+    asíncrono y nos mantiene a salvo de problemas y errores.
+
  */
